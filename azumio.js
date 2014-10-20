459588856fd6cb9c0a9c32569ac45e8c8b7b6e71
Sam Day <sday@atlassian.com>
2014-10-20 23:40:42 +1100

    Halfway through porting to use traceurified-module

--- azumio.js
+++ azumio.js
@@ -1,18 +1,85 @@
 "use strict";
 
-// This is the library entrypoint.
-// If there exists an azumio.dist.js in the current directory, we delegate to that. Otherwise, it's assumed we're in
-// dev mode, wherein we delegate to the raw azumio-es6.js module, using Traceurified to shim in Traceur.
+var Promise = require("bluebird");
+var request = Promise.promisifyAll(require("request"));
+var tough = require("tough-cookie");
 
-var fs = require("fs"),
-    path = require("path");
+var defaultBaseUrl = "http://api.azumio.com";
 
-if (fs.existsSync(path.join(__dirname, "azumio.dist.js"))) {
-  require("traceur/bin/traceur-runtime");
-  module.exports = require("./azumio.dist");
-} else {
-  require("traceurified")(function(file) {
-    return file.indexOf("node_modules") < 0;
-  });
-  module.exports = require("./azumio.es6");
+class Azumio {
+  constructor(email, password, baseUrl = defaultBaseUrl) {
+    if (!email || !password) {
+      throw new Error("Email and password are required.");
+    }
+
+    this.baseUrl = baseUrl;
+    this.email = email;
+    this.authToken = false;
+
+    this._login = Promise.coroutine(function*() {
+      var [response] = yield request.postAsync(`${this.baseUrl}/login`, {
+        form: {
+          email: this.email,
+          password: password
+        }
+      });
+
+      // Look for the oath_token_azumio cookie. If it's not there,
+      // login was incorrect, or something is wrong.
+      var cookies = response.headers["set-cookie"];
+      var oauthToken = cookies
+        .map(rawCookie => tough.parse(rawCookie))
+        .find(cookie => cookie.key === "oath_token_azumio")
+        .value;
+
+      if (!oauthToken) {
+        throw new Error("Login failed. Check login details and service status.");
+      }
+
+      this.authToken = oauthToken;
+    }.bind(this));
+
+    this._request = Promise.coroutine(function*(uri, qs) {
+      if (!this.authToken) {
+        yield this._login();
+      }
+
+      var [response] = yield request.getAsync(`${this.baseUrl}${uri}`, {
+        qs,
+        headers: {
+          Cookie: `oath_token_azumio=${this.authToken}`,
+          "Content-Type": "application/json",
+        },
+        json: true,
+      });
+
+      return response.body;
+    }.bind(this));
+  }
+
+  // TODO: figure out how offsetting works. Wrap response in a proper container.
+  heartrate(callback) {
+    var responsePromise = this._request("/v2/checkins.csv", {
+      _format: "json",
+      _fields: ["id", "timestamp", "type", "value", "note", "tags"].join(","),
+      type: "heartrate",
+      limit: 2000,
+      _expand: "none",
+    });
+
+    return responsePromise.then(function(response) {
+      var data = response.checkins.map(checkin => ({
+        id: checkin.id,
+        tags: checkin.tags,
+        timestamp: checkin.timestamp,
+        rate: checkin.value
+        // TODO: notes?
+      }));
+
+      data.hasMore = response.hasMore;
+      return data;
+    }).nodeify(callback);
+  }
 }
+
+module.exports = Azumio;
